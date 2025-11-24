6ab0130a09ef607a552d04ccd90e8825f40f8733
David Lee <mikeportnoy168@gmail.com>
2025-11-24 19:01:45 +0800

    feat: add NetworkService

--- CryptoTracker/Packages/NetworkingKit/Sources/NetworkingKit/NetworkService.swift
+++ CryptoTracker/Packages/NetworkingKit/Sources/NetworkingKit/NetworkService.swift
@@ -41,3 +41,47 @@ public struct Endpoint {
     URL(string: "\(baseURL)\(path)")
   }
 }
+
+public protocol NetworkServiceProtocol {
+  func fetch<T: Decodable>(_ endpoint: Endpoint) -> AnyPublisher<T, NetworkError>
+}
+
+public class NetworkService: NetworkServiceProtocol {
+  private let session: URLSession
+  
+  public init(session: URLSession = .shared) {
+    self.session = session
+  }
+  
+  public func fetch<T: Decodable>(_ endpoint: Endpoint) -> AnyPublisher<T, NetworkError> {
+    guard let url = endpoint.url else {
+      return Fail(error: NetworkError.invalidURL).eraseToAnyPublisher()
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = endpoint.method
+    request.timeoutInterval = 30
+    endpoint.headers?.forEach { request.setValue($0.value, forHTTPHeaderField: $0.key) }
+    
+    return session.dataTaskPublisher(for: request)
+      .tryMap { output in
+        guard let httpResponse = output.response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+          throw NetworkError.invalidResponse
+        }
+        return output.data
+      }
+      .decode(type: T.self, decoder: JSONDecoder())
+      .mapError { error in
+        if let networkError = error as? NetworkError {
+          return networkError
+        } else if error is DecodingError {
+          return NetworkError.decodingFailed(error)
+        } else {
+          return NetworkError.requestFailed(error)
+        }
+      }
+      .receive(on: DispatchQueue.main)
+      .eraseToAnyPublisher()
+  }
+}
