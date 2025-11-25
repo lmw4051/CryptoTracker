c57ff9403f9f116db936f48b4fe7ca8b6fcaf1b4
David Lee <mikeportnoy168@gmail.com>
2025-11-25 13:56:10 +0800

    feat: add CryptoList

---
+++ CryptoTracker/Accessibility/AccessibilityIdentifiers.swift
@@ -0,0 +1,23 @@
+//
+//  AccessibilityIdentifiers.swift
+//  CryptoTracker
+//
+//  Created by David Lee on 11/25/25.
+//
+
+import Foundation
+
+public enum AccessibilityID {
+  // MARK: - Crypto List
+  public enum CryptoList {
+    public static let view = "cryptoListView"
+    public static let table = "cryptoListTable"
+    public static let refreshButton = "refreshButton"
+    public static let loadingIndicator = "loadingIndicator"
+    public static let errorMessage = "errorMessage"
+    
+    public static func row(_ id: String) -> String {
+      "cryptoRow_\(id)"
+    }
+  }
+}
