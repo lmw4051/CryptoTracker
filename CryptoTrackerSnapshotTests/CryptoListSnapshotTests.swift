191391b942d016e8dd566ef9150711dd3d5b65e8
David Lee <mikeportnoy168@gmail.com>
2025-11-29 14:42:36 +0800

    test: add testCryptoRowViewLight method

--- CryptoTrackerSnapshotTests/CryptoListSnapshotTests.swift
+++ CryptoTrackerSnapshotTests/CryptoListSnapshotTests.swift
@@ -15,4 +15,20 @@ final class CryptoTrackerSnapshotTests: XCTestCase {
   override func setUp() {
     super.setUp()
   }
+  
+  func testCryptoRowViewLight() {
+    let crypto = Crypto(
+      id: "bitcoin",
+      symbol: "btc",
+      name: "Bitcoin",
+      currentPrice: 50000.00,
+      priceChangePercentage24h: 5.25
+    )
+    
+    let view = CryptoRowView(crypto: crypto)
+      .frame(width: 375, height: 80)
+      .background(Color.white)
+    
+    assertSnapshot(of: view, as: .image(precision: 0.99))
+  }
 }
